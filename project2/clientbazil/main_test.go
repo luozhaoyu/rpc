0b634d12360b27fc2c0cbc0067b7022ab64fbd58
luozhaoyu <luozhaoyu90@gmail.com>
2015-10-31 15:19:19 -0600

    add clientside read write offset test

--- project2/clientbazil/main_test.go
+++ project2/clientbazil/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"testing"
 )
@@ -13,25 +14,86 @@ func CheckError(err error, t *testing.T) {
 	}
 }
 
-func TestFileCRUD(t *testing.T) {
-	//	out, err = exec.Command("ls", "-l").Output()
-	//	t.Log(string(out))
+func TestTouchAndWriteFile(t *testing.T) {
 	out, err := exec.Command("rm", "-rf", "root/*").Output()
 	out, err = exec.Command("touch", "root/a").Output()
 	CheckError(err, t)
 	err = ioutil.WriteFile("root/a", []byte("FileCRUD seems OK"), 0644)
 	CheckError(err, t)
 	out, err = exec.Command("cat", "root/a").Output()
-	CheckError(err, t)
 	t.Log(string(out))
+	CheckError(err, t)
 }
 
-func TestFolderCRUD(t *testing.T) {
+func TestMkdir(t *testing.T) {
 	out, err := exec.Command("rm", "-rf", "root/*").Output()
 	CheckError(err, t)
 	out, err = exec.Command("mkdir", "-p", "root/b/b").Output()
 	CheckError(err, t)
-	out, err = exec.Command("ls", "-l", "root/b/b").Output()
-	CheckError(err, t)
+	out, err = exec.Command("stat", "root/b/b").Output()
 	t.Log(string(out))
+	CheckError(err, t)
+}
+
+func TestReadAt(t *testing.T) {
+	err := ioutil.WriteFile("root/test_read_at", []byte("123456789"), 0644)
+	CheckError(err, t)
+	f, err := os.OpenFile("root/test_read_at", os.O_RDWR, 0644)
+	CheckError(err, t)
+	var readData []byte
+	var n int
+	readData = make([]byte, 3)
+	n, err = f.ReadAt(readData, 1)
+	t.Log("ReadAt", n, err, string(readData))
+	CheckError(err, t)
+	if n != len(readData) || string(readData) != "234" {
+		t.FailNow()
+	}
+
+}
+
+func TestWriteAt(t *testing.T) {
+	var n int
+	fileName := "root/test_write_at"
+	err := ioutil.WriteFile(fileName, []byte("123456789"), 0644)
+	CheckError(err, t)
+	f, err := os.OpenFile(fileName, os.O_RDWR, 0644)
+	CheckError(err, t)
+
+	writeData := []byte("abc")
+	n, err = f.WriteAt(writeData, 2)
+	CheckError(err, t)
+	err = f.Sync()
+	CheckError(err, t)
+
+	out, err := exec.Command("cat", fileName).Output()
+	t.Log("WriteAt", n, err, string(out))
+	CheckError(err, t)
+
+	if n != len(writeData) || string(out) != "12abc6789" {
+		t.FailNow()
+	}
+}
+
+func TestWriteAtExceed(t *testing.T) {
+	var n int
+	fileName := "root/test_write_at_exceed"
+	err := ioutil.WriteFile(fileName, []byte("123456789"), 0644)
+	CheckError(err, t)
+	f, err := os.OpenFile(fileName, os.O_RDWR, 0644)
+	CheckError(err, t)
+
+	writeData := []byte("abcdef")
+	n, err = f.WriteAt(writeData, 7)
+	CheckError(err, t)
+	err = f.Sync()
+	CheckError(err, t)
+
+	out, err := exec.Command("cat", fileName).Output()
+	t.Log("WriteAtExceed", n, err, string(out))
+	CheckError(err, t)
+
+	if n != len(writeData) || string(out) != "1234567abcdef" {
+		t.FailNow()
+	}
 }
