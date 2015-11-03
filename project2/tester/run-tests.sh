3c93559e38bd308d4188043ddbeabd28912f4425
luozhaoyu <luozhaoyu90@gmail.com>
2015-11-03 10:15:52 -0700

    Fixed tester bug.

--- project2/tester/run-tests.sh
+++ project2/tester/run-tests.sh
@@ -6,8 +6,8 @@ ROOT=../clientbazil/root
 
 for i in 2k 4k 16k 256k 4m 16m 64m
 do
- $EXE SingleAccess $ROOT/blob-$i-t0 1
- $EXE SingleAccess $ROOT/blob-$i-t0 20
- $EXE SingleReadWrite $ROOT/blob-$i-t0 r 5
- $EXE SingleReadWrite $ROOT/blob-$i-t0 w 5
+ $EXE SingleAccess $ROOT/blob-$i 1
+ $EXE SingleAccess $ROOT/blob-$i 20
+ $EXE SingleReadWrite $ROOT/blob-$i r 5
+ $EXE SingleReadWrite $ROOT/blob-$i w 5
 done
